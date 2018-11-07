8ba658c6fc1e404f6f607def26d60058e422fd1b
ko_kurebayashi <kureykou321@gmail.com>
2018-11-07 00:49:23 +0000

    want

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+ before_action :require_user_logged_in, only: [:show]
+ 
+ 
+  def show
+    @user=User.find(params[:id])
+    @items=@user.items.uniq
+    @count_want=@user.want_items.count
+  end
+  
+  def new
+    @user=User.new
+  end
+
+  def create
+     @user=User.new(user_params)
+     
+     if @user.save
+      flash[:success]='ユーザーの登録が完了しました'
+      redirect_to @user
+    else
+      flash.now[:danger]= 'ユーザーの登録が失敗しました'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+ 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
+
